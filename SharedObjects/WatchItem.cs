a5c7e488111dd2a959d7d50e9603a0fa949854ac
Kevin Burgon <buburgo@gmail.com>
2020-03-21 19:22:48 -0600

    Creates initial project files

---
+++ SharedObjects/WatchItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharedObjects
+{
+    public class WatchItem
+    {
+        public int WatchItemID { get; set; }
+        public string WatchItemName { get; set; }
+        public string Url { get; set; }
+        public string ItemPath { get; set; }
+    }
+}
---
+++ WatchItemStore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WatchItemStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
