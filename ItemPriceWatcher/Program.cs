0aee37dd119f833a3587f3ac331f5acf645a2153
Kevin Burgon <buburgo@gmail.com>
2020-03-28 10:10:13 -0600

    Adds TxtWatchItemAccess class

--- ItemPriceWatcher/Program.cs
+++ ItemPriceWatcher/Program.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using WatchItemData;
+﻿using WatchItemData.WatchItemAccess;
 
 namespace ItemPriceWatcher
 {
     class Program
     {
-        static IWorkItemAccess itemGet;
+        static IWatchItemAccess itemGet;
 
         static void Main(string[] args)
         {
             if (args.Length > 1 && args[1] == "--txtsource")
             {
-                itemGet = new SqlWorkItemAccess();
+                itemGet = new SqlWatchItemAccess();
             }
         }
     }
---
+++ WatchItemData/WatchItemAccess/TxtWatchItemAccess.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace WatchItemData.WatchItemAccess
+{
+    class TxtWatchItemAccess : IWatchItemAccess
+    {
+        private string fullFilePath;
+        
+        /// <summary>
+        /// Constructor that sets the file path from the given <paramref name="path">.
+        /// </summary>
+        /// <param name="path">The full file path of the file containing watch items</param>
+        public TxtWatchItemAccess(string path)
+        {
+            fullFilePath = path;
+        }
+
+        public List<WatchItem> GetActiveWatchItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
