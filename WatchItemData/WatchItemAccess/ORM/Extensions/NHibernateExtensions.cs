64a1f5438d34aa55322033cdf5c919fc0a8672c9
Kevin Burgon <buburgo@gmail.com>
2020-05-25 17:16:17 -0600

    Adds dependency injection for ORM

---
+++ WatchItemData/WatchItemAccess/ORM/Extensions/NHibernateExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Dialect;
+using NHibernate.Mapping.ByCode;
+using WatchItemData.WatchItemAccess.ORM.Sessions;
+
+namespace WatchItemData.WatchItemAccess.ORM.Extensions
+{
+    public static class NHibernateExtensions
+    {
+        public static IServiceCollection AddNHibernate<T>(this IServiceCollection services, string connectionString)
+        {
+            var mapper = new ModelMapper();
+            mapper.AddMappings(typeof(NHibernateExtensions).Assembly.ExportedTypes);
+            HbmMapping domainMapping = mapper.CompileMappingForAllExplicitlyAddedEntities();
+
+            var configuration = new Configuration();
+            configuration.DataBaseIntegration(c => 
+            {
+                c.Dialect<MySQL57Dialect>();
+                c.ConnectionString = connectionString;
+                c.KeywordsAutoImport = Hbm2DDLKeyWords.AutoQuote;
+                c.SchemaAction = SchemaAutoAction.Validate;
+                c.LogFormattedSql = true;
+                c.LogSqlInConsole = true;
+            });
+
+            configuration.AddMapping(domainMapping);
+
+            var sessionFactory = configuration.BuildSessionFactory();
+
+            services.AddSingleton(sessionFactory);
+            services.AddScoped(factory => sessionFactory.OpenSession());
+            services.AddScoped<IMapperSession<T>, MapperSession<T>>();
+
+            return services;
+        }
+    }
+}
