e4f846a2b68d38bbe96dbc65d63a8beb25ca9f33
Kevin Burgon <kevin.burgon@gmail.com>
2020-06-20 16:33:05 -0600

    Adds application configuration

--- ItemPriceWatcher/Program.cs
+++ ItemPriceWatcher/Program.cs
@@ -1,38 +1,69 @@
 ﻿using System;
-using System.Linq;
-using WatchItemData.WatchItemAccess;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ItemPriceWatcher
 {
     class Program
     {
         // static IWatchItemAccess itemGet;
+        public static IConfigurationRoot configuration;
 
         static void Main(string[] args)
         {
-            string username = string.Empty;
-            string password = string.Empty;
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+            Log.Debug("Logger initialized");
 
-            for (int i = 0; i < args.Length; i++)
+            try
             {
-                if (args[i] == "-u")
-                {
-                    username = args[i+1];
-                }
-                else if (args[i] == "-p")
-                {
-                    password = args[i+1];
-                }
+                RunApplication();
             }
-
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            catch (Exception e)
             {
-                Console.WriteLine("A username or password is required.");
-                return;
+                Log.Error("Application error encountered", e);
             }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
 
-            EmailSender sender = new EmailSender(username, password);
-            sender.SendMail(username, "TEST EMAIL", "Hello, world!");
+        private static void RunApplication()
+        {
+            Log.Information("Starting application");
+            Log.Information("Configuring services");
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            Log.Information("Building service provider");
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var connectionString = configuration.GetConnectionString("Development");
+            Console.WriteLine(connectionString);
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton(LoggerFactory.Create(builder => 
+            {
+                builder.AddSerilog(dispose: true);
+            }));
+
+            serviceCollection.AddLogging();
+
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Add access to generic IConfigurationRoot
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
         }
     }
 }
