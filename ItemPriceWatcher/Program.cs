4aac7f79b1360bdc29985f1a40c6b9597f760525
Kevin Burgon <kevin.burgon@gmail.com>
2020-09-07 09:09:58 -0600

    Cleans up unused usings

--- ItemPriceWatcher/Program.cs
+++ ItemPriceWatcher/Program.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WatchItemData;
