2414aba9e1e4a7d09a31bfcd3cf9079a073e0cb8
Kevin Burgon <kevin.burgon@gmail.com>
2021-09-27 22:01:04 -0600

    Remove old projects, add test project
    Removes all the folders and files associated with the original version
    of the app.  Creates the new project for nunit tests.

--- ItemPriceWatcher.Manager/Models/WatchItem/WatchItemCollectionViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ItemPriceWatcher.Manager.Models.WatchItem
-{
-    public class WatchItemCollectionViewModel
-    {
-        public List<WatchItemViewModel> WatchItems { get; set; } = new List<WatchItemViewModel>();
-    }
-}
