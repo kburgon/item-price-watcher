3c82312ff3559ce0dbd010982781e5442fd809d5
Kevin Burgon <kevin.burgon@gmail.com>
2021-10-27 21:49:05 -0600

    WIP: Add username to DB setup scripts

---
+++ Database/db_user_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER pricewatcheruser ENCRYPTED PASSWORD '$POSTGRES_PASSWORD'
+EOSQL
--- Database/setup.sql
+++ Database/setup.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE pricewatcher;
 \c pricewatcher;
-CREATE SCHEMA pricewatcher;
+-- CREATE SCHEMA pricewatcher;
 CREATE TABLE test_table (id INT GENERATED ALWAYS AS IDENTITY, value VARCHAR NOT NULL);
+CREATE USER pricewatcheruser ENCRYPTED PASSWORD 'testpassword';
+GRANT CONNECT ON DATABASE pricewatcher TO pricewatcheruser;
+GRANT SELECT, INSERT ON ALL TABLES IN SCHEMA public TO pricewatcheruser;
